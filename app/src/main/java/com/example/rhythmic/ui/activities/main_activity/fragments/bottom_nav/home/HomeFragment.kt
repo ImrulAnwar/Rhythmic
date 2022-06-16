a1f1c5df4d6a0586f50f2f78478b6cec0ee98df9
Imrul <imrulanwar836@gmail.com>
2022-06-16 18:16:16 +0600

    added the sort button animations

--- app/src/main/java/com/example/rhythmic/ui/activities/main_activity/fragments/bottom_nav/home/HomeFragment.kt
+++ app/src/main/java/com/example/rhythmic/ui/activities/main_activity/fragments/bottom_nav/home/HomeFragment.kt
@@ -4,6 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
@@ -32,6 +35,15 @@ class HomeFragment : Fragment() {
         private val adapter: VerticalAdapter by lazy { VerticalAdapter(requireActivity(), TAG) }
 //        @Inject lateinit var artistFragment: ArtistFragment
 //        @Inject lateinit var albumFragment: AlbumFragment
+        private var clicked: Boolean= false
+        private val rotateClockwise: Animation by lazy{ AnimationUtils.loadAnimation(requireContext(), R.anim.rotate_clockwise)}
+        private val rotateCounterClockwise: Animation by lazy{ AnimationUtils.loadAnimation(requireContext(), R.anim.rotate_counter_clockwise)}
+        private val toTop: Animation by lazy{ AnimationUtils.loadAnimation(requireContext(), R.anim.to_top)}
+        private val toBottom: Animation by lazy{ AnimationUtils.loadAnimation(requireContext(), R.anim.to_bottom)}
+        private val toLeft: Animation by lazy{ AnimationUtils.loadAnimation(requireContext(), R.anim.to_left)}
+        private val toRight: Animation by lazy{ AnimationUtils.loadAnimation(requireContext(), R.anim.to_right)}
+        private val toTopLeft: Animation by lazy{ AnimationUtils.loadAnimation(requireContext(), R.anim.to_top_left)}
+        private val toBottomRight: Animation by lazy{ AnimationUtils.loadAnimation(requireContext(), R.anim.to_bottom_right)}
         private val fm: FragmentManager by  lazy { requireActivity().supportFragmentManager }
 
         override fun onCreateView(
@@ -52,10 +64,51 @@ class HomeFragment : Fragment() {
                 allSongs.observe(viewLifecycleOwner) {
                         allSongs.value?.let { adapter.setData(it) }
                 }
+                binding.fabSort.setOnClickListener {
+                        setVisibility(clicked)
+                        setAnimation(clicked)
+                        clicked = !clicked
+                }
+                binding.fabByDate.setOnClickListener {
+                        Toast.makeText(requireContext(), "By Date", Toast.LENGTH_SHORT).show()
+                }
 
+                binding.fabByName.setOnClickListener {
+                        Toast.makeText(requireContext(), "By Name", Toast.LENGTH_SHORT).show()
+                }
+
+                binding.fabBySize.setOnClickListener {
+                        Toast.makeText(requireContext(), "By Size", Toast.LENGTH_SHORT).show()
+                }
                 handleAlbumAndArtistButtonClicks()
 
                 return root
+        }
+
+        private fun setAnimation(clicked: Boolean) {
+                if (!clicked) {
+                        binding.fabBySize.startAnimation(toLeft)
+                        binding.fabByName.startAnimation(toTop)
+                        binding.fabByDate.startAnimation(toTopLeft)
+                        binding.fabSort.startAnimation(rotateCounterClockwise)
+                } else {
+                        binding.fabBySize.startAnimation(toRight)
+                        binding.fabByName.startAnimation(toBottom)
+                        binding.fabByDate.startAnimation(toBottomRight)
+                        binding.fabSort.startAnimation(rotateClockwise)
+                }
+        }
+
+        private fun setVisibility(clicked: Boolean) {
+                if (!clicked) {
+                        binding.fabBySize.visibility = View.VISIBLE
+                        binding.fabByName.visibility = View.VISIBLE
+                        binding.fabByDate.visibility = View.VISIBLE
+                } else {
+                        binding.fabBySize.visibility = View.INVISIBLE
+                        binding.fabByName.visibility = View.INVISIBLE
+                        binding.fabByDate.visibility = View.INVISIBLE
+                }
         }
 
         override fun onDestroyView() {
